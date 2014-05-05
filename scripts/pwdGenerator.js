6bac32c86ccef10a3fda5131ce26e03a30c5b28d
Christian <christian.etpr10@gmail.com>
2014-05-05 19:39:00 -0400

    Added script to generate hours password

---
+++ scripts/pwdGenerator.js
@@ -0,0 +1,35 @@
+var generator = require('password-generator');
+// Database
+var mongo = require('mongoskin');
+var mongoUri = process.env.MONGOHQ_URL  || 'mongodb://localhost:27017/interpretelocal';
+var db = mongo.db(mongoUri);
+db.ObjectID = mongo.ObjectID;
+
+if (process.argv.length !== 4) {
+  console.log("Usage: node pwdGenerator.js amountOfPasswords amountOfHours");
+  process.exit(1);
+};
+
+var amount = process.argv[2],
+    hours  = process.argv[3],
+    inserted = 0,
+    data = [];
+
+for (var i = amount - 1; i >= 0; i--) {
+  data.push({
+    password: generator(15),
+    hours: hours,
+    used: false,
+    user_id: null,
+    created_date: new Date()
+  });
+};
+
+db.collection('passwords').insert(data, function(err, result) {
+  if (err) {
+    console.log("DB Error: %s", err)
+  } else{
+    console.log('Inserted all passwords. Check the db :)');
+    process.exit();
+  };
+});
